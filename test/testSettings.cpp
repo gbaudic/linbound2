88d056ff5401640dd3b26190662505e144aa59a9
Gwilherm Baudic <gbaudic@users.noreply.github.com>
2018-12-03 17:24:22 +0100

    First step with google test [ci skip]

---
+++ test/testSettings.cpp
@@ -0,0 +1,22 @@
+#include "../settings.hpp"
+#include "gtest/gtest.h" 
+
+
+namespace {
+    
+    TEST(SettingsTest, DefaultWidth) {
+        Settings* set = Settings::getInstance();
+        EXPECT_EQ(set->getWidth(), 800);
+    }
+    
+    TEST(SettingsTest, DefaultHeight) {
+        Settings* set = Settings::getInstance();
+        EXPECT_EQ(set->getHeight(), 600);
+    }
+    
+}
+
+int main(int argc, char **argv) {
+  ::testing::InitGoogleTest(&argc, argv);
+  return RUN_ALL_TESTS();
+}
